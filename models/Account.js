46a728dc9d0d0afe2cba8ec9db89a50e44172439
ebubekeyz <ebubeofforjoe@gmail.com>
2024-07-11 22:13:45 +0100

    1

---
+++ models/Account.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+const validator = require('validator');
+
+const AccountSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    bank: {
+      type: String,
+      required: true,
+    },
+    accountNumber: {
+      type: String,
+      required: true,
+    },
+    pin: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      default: moment().format('YYYY-DD-MM'),
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Account', AccountSchema);
